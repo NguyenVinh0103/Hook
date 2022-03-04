39c56a82b5c91477dbf8068748ad11b4cf405523
NguyenVinh <vinhnqgcs17528@gmail.com>
2022-03-04 10:06:37 -0800

    OnewayBinding

--- App.js
+++ App.js
@@ -2,10 +2,12 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import State from './src/Screen/State/index.js';
 import Effect from './src/Screen/Effect/index.js';
+import LoginInput from './src/Screen/LoginInput/index.js';
 
 const App = () => {
   return (
     <View>
+      <LoginInput />
       <State/>
       <Effect/>
     </View>
